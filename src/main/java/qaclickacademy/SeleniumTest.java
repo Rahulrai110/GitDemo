49c194899dc8d5f01f46dac2169104e9aa73a045
Rahul <rahul.rai606@gmail.com>
2021-04-18 19:36:57 +0530

    This is from American timezone guy

--- src/main/java/qaclickacademy/SeleniumTest.java
+++ src/main/java/qaclickacademy/SeleniumTest.java
@@ -7,7 +7,8 @@ public class SeleniumTest {
 	@Test
 	public void BrowserAutomation()
 	{
-		System.out.println("BrowserAutomation");		
+		System.out.println("BrowserAutomation");
+		System.out.println("WebAutomation");
 	}
 	
 	@Test
